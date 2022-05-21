07e07262c5a9d18e6f4cd2f7718b4092a1e6cb78
Zohar Zilberman <popen2@gmail.com>
2022-05-21 12:54:42 +0300

    Status API

---
+++ src/api.rs
@@ -0,0 +1,24 @@
+use actix_web::{web, HttpResponse, Responder};
+use platz_sdk::deployment_status::{PlatzStatus, Status, StatusColor};
+use serde::Serialize;
+
+pub fn configure(cfg: &mut web::ServiceConfig) {
+    cfg.route("/status", web::get().to(status));
+}
+
+#[derive(Clone, Serialize)]
+enum ShopStatus {
+    Open,
+}
+
+async fn status() -> impl Responder {
+    HttpResponse::Ok().json(PlatzStatus {
+        status: Status {
+            name: ShopStatus::Open,
+            color: StatusColor::Success,
+        },
+        primary_metric: None,
+        metrics: None,
+        notices: Default::default(),
+    })
+}
